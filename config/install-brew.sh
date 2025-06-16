9e430dd9bea4d4dd2475a180a9abdc4de36468f1
kongsakchai <ksakchai.dev@gmail.com>
2025-06-16 15:03:14 +0700

    :tada: update Brewfile

--- config/install-brew.sh
+++ config/install-brew.sh
@@ -1,5 +1,6 @@
 echo "Installing Homebrew and packages..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
 echo "Setting up Homebrew environment..."
 brew bundle --file ~/Brewfile
--- config/software-list.sh
+++ config/software-list.sh
@@ -1,3 +1,2 @@
 open "https://go.dev/doc/install"
-open "https://obsidian.md/download"
 open "https://podman.io/"
