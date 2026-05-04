0d51cc09ce94dd5b25c1fb237f2638a7fbd4bbdd
Sakchai Paoin <55359787+kongsakchai@users.noreply.github.com>
2026-05-04 23:49:02 +0700

    :wrench: fix fzf directory

--- setup/linux/05-install-fzf.sh
+++ setup/linux/05-install-fzf.sh
@@ -6,7 +6,7 @@ else
 	echo "🚀 installed fzf..."
 	
 	mkdir -p ~/bin/fzf
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	~/.fzf/install
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/bin/fzf
+	~/bin/fzf/install
     echo "✅ fzf installed!"
 fi
