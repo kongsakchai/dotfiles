42ede508ce746ce2bad41920038ba57dfee8027a
kongsakchai <ksakchai.dev@gmail.com>
2026-08-22 15:37:53 +0700

    :tada: update fish function

--- config/dot_config/private_fish/functions/gme.fish
+++ config/dot_config/private_fish/functions/gme.fish
@@ -1,6 +1,6 @@
 function gme
 	set -l branch (gb)
 	if test -n "$branch"
-		git merge "$branch" "$argv"
+		git merge "$branch" $argv
 	end
 end
--- config/dot_config/private_fish/functions/gph.fish
+++ config/dot_config/private_fish/functions/gph.fish
@@ -1,3 +1,3 @@
-function gg
+function gph
 	git log --graph --all --pretty=format:"%C(yellow)%h%C(reset) %C(auto)%d%C(reset) %C(cyan)[%an]%C(reset) %s"
 end
