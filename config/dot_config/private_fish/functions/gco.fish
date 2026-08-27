13ad97976f9a8514ec584775e95a683cc52399fa
kongsakchai <ksakchai.dev@gmail.com>
2026-08-28 04:53:28 +0700

    :wrench: fix command $args -> $argv

--- config/dot_config/private_fish/functions/gco.fish
+++ config/dot_config/private_fish/functions/gco.fish
@@ -1,6 +1,6 @@
 function gco
 	set -l branch (gb)
 	if test -n "$branch"
-		git checkout $args "$branch"
+		git checkout $argv "$branch"
 	end
 end
--- config/dot_config/private_fish/functions/gocover.fish
+++ config/dot_config/private_fish/functions/gocover.fish
@@ -1,4 +1,4 @@
 function gocover
-    go test -coverprofile=coverage.out $args[1]
+    go test -coverprofile=coverage.out $argv
     go tool cover -html=coverage.out
 end
